3c10f8880831af2a4139d97b3f7f763f4592f776
Mohamed Mafaz <119058966+Mohamed-Mafaz@users.noreply.github.com>
2022-11-26 14:07:50 +0530

    Add files via upload
    Mapping components in react

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "./Card";
+import emojipedia from "../emojipedia";
+
+function createCard(emoji) {
+  return (
+    <div>
+      <Card
+        key={emoji.id}
+        emoji={emoji.emoji}
+        name={emoji.name}
+        meaning={emoji.meaning}
+      ></Card>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+    </div>
+  );
+}
+
+export default App;
